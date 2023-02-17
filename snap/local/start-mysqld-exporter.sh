c24a218b61127fffdb1c9a0de687447aaee7d410
jarred wilson <20207921+jardon@users.noreply.github.com>
2023-02-17 11:54:09 -0500

    Add exporter components (#3)
    * Add exporter components
    
    * Add default client config for exporter

---
+++ snap/local/start-mysqld-exporter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# For security measures, daemons should not be run as sudo. Execute mysqlrouter as the non-sudo user: snap-daemon.
+$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
+  --regid snap_daemon -- $SNAP/bin/mysqld_exporter --config.my-cnf=$SNAP_COMMON/mysql/mysql.cnf "$@"
+
