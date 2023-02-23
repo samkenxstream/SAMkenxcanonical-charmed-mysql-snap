5bd172758022f65713561502315da10853097c6a
jarred wilson <20207921+jardon@users.noreply.github.com>
2023-02-23 14:58:55 -0500

    Rework snap hooks (#5)
    * Rework permissions handling
    
    * Move configuration setup to an install hook instead of configure hook
    
    * Add mysqlx socket config
    
    * Update paths to defaults file
    
    * Add exec to wrappers

--- snap/local/start-mysql-router.sh
+++ snap/local/start-mysql-router.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # For security measures, daemons should not be run as sudo. Execute mysqlrouter as the non-sudo user: snap-daemon.
-$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
+exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
   --regid snap_daemon -- $SNAP/usr/bin/mysqlrouter "$@"
--- snap/local/start-mysqld.sh
+++ snap/local/start-mysqld.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
-  --regid snap_daemon -- $SNAP/usr/sbin/mysqld --defaults-file=${SNAP_COMMON}/mysql/mysql.conf.d/mysqld.cnf
+exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
+  --regid snap_daemon -- $SNAP/usr/sbin/mysqld --defaults-file=${SNAP_COMMON}/mysql/mysql.cnf "$@"
