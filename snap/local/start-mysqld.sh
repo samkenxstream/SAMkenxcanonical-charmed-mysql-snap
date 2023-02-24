c9c94d74ecc1c54991d0f96e3cc564f2ef74bd67
Jarred Wilson <jarred.wilson12@gmail.com>
2023-02-24 10:56:26 -0500

    Move to mysqld_safe in start script

--- snap/local/start-mysqld.sh
+++ snap/local/start-mysqld.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
-  --regid snap_daemon -- $SNAP/usr/sbin/mysqld --defaults-file=${SNAP_COMMON}/mysql/mysql.cnf "$@"
+  --regid snap_daemon -- $SNAP/usr/bin/mysqld_safe --defaults-file=${SNAP_COMMON}/mysql/mysql.cnf "$@"
