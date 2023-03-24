f88ab48f0627d35d8595591795eed5f6454f8781
jarred wilson <20207921+jardon@users.noreply.github.com>
2023-03-24 10:24:37 -0400

    Rework snap directories (#11)
    * Initial dir rework

--- snap/local/start-mysqld-exporter.sh
+++ snap/local/start-mysqld-exporter.sh
@@ -2,5 +2,5 @@
 
 # For security measures, daemons should not be run as sudo. Execute mysqlrouter as the non-sudo user: snap-daemon.
 exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
-  --regid snap_daemon -- $SNAP/bin/mysqld_exporter --config.my-cnf=$SNAP_COMMON/mysql/mysql.cnf "$@"
+  --regid snap_daemon -- $SNAP/bin/mysqld_exporter --config.my-cnf=$SNAP_DATA/etc/mysql/mysql.cnf "$@"
 
--- snap/local/start-mysqld.sh
+++ snap/local/start-mysqld.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
-  --regid root -- $SNAP/usr/bin/mysqld_safe --defaults-file=${SNAP_COMMON}/mysql/mysql.cnf "$@"
+  --regid root -- $SNAP/usr/bin/mysqld_safe --defaults-file=$SNAP_DATA/etc/mysql/mysql.cnf "$@"
