a116aee9be996fe14686f3aed58d43499c3ee436
Paulo Machado <paulo.machado@canonical.com>
2023-03-29 16:07:09 -0300

    enable snap configuration and migrate exporter configuration to wrapper

--- snap/local/start-mysqld-exporter.sh
+++ snap/local/start-mysqld-exporter.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-# For security measures, daemons should not be run as sudo. Execute mysqlrouter as the non-sudo user: snap-daemon.
-exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
-  --regid snap_daemon -- $SNAP/bin/mysqld_exporter --config.my-cnf=$SNAP_DATA/etc/mysql/mysql.cnf "$@"
+MYSQLD_EXPORTER_PASSWORD="$(snapctl get exporter_user_password)"
 
+# For security measures, daemons should not be run as sudo.
+# Execute mysqlrouter as the non-sudo user: snap-daemon.
+exec $SNAP/usr/bin/setpriv \
+    --clear-groups \
+    --reuid snap_daemon \
+    --regid snap_daemon -- \
+    "$SNAP/bin/mysqld_exporter" \
+        --collect.auto-increment.columns=false \
+        --collect.info-schema.tables=false \
+        --collect.info-schema.tablestats=false \
+        --collect.perf-schema.indexiowaits=false \
+        --collect.perf-schema.tableiowaits=false \
+        --collect.perf-schema.tablelocks=false \
+        --collect.info-schema.userstats=false \
+        --collect.binlog-size=false \
+        --collect.info-schema.processlist=false \
+        --mysql.socket="$SNAP_COMMON/var/run/mysqld/mysqld.sock" \
+        --mysqld.username="$(snapctl get exporter_user)" \
