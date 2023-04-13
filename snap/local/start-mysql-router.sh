763edfda3b71533bcecf4744f81d94984fd2337b
Shayan Patel <shayan.patel@canonical.com>
2023-04-13 16:27:34 -0400

    Make mysqlrouter applications work + restrict plugs and layouts for mysql applications

--- snap/local/start-mysql-router.sh
+++ snap/local/start-mysql-router.sh
@@ -2,4 +2,4 @@
 
 # For security measures, daemons should not be run as sudo. Execute mysqlrouter as the non-sudo user: snap-daemon.
 exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
-  --regid snap_daemon -- $SNAP/usr/bin/mysqlrouter "$@"
+  --regid root -- $SNAP/usr/bin/mysqlrouter --config $SNAP_DATA/etc/mysqlrouter/mysqlrouter.conf
